7bb99f31e8bdf58ae006be9bf9f3f1ed1076ed78
lhugg22 <lhugg22@gmail.com>
2020-12-17 21:36:56 -0500

    updating code... not done yet...

--- ArdNano-Xmas-WS2812.ino
+++ ArdNano-Xmas-WS2812.ino
@@ -1,5 +1,26 @@
+//Louis Huggins
+
+
+#include <HCSR04.h>
+#include <Adafruit_NeoPixel.h>
+
+#define trigPin 3
+#define echoPin 4
+
+#define LED_PIN 8
+#define LED_COUNT 60
+
+AdaFruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+int state_var, alt;
+long duration, cm;
+bool lights_on, changed;
 
 void setup() {
+
+	pinMode(trigPin, OUTPUT);
+	pinMode(echoPin, INPUT);
+
 }
 
 void loop() {
